27f43471d8bf43a9687408c45ee56606abc191f3
SayamMahajan <mahajansayam3@gmail.com>
2024-08-10 12:13:02 +0530

    1431_Kids_With_the_Greatest_Number_of_Candies

---
+++ Problems/1431_Kids_With_the_Greatest_Number_of_Candies.cpp
@@ -0,0 +1,26 @@
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        vector<bool> ans;
+        int max = 0;
+
+        for(int i =0 ; i < candies.size(); i++){
+            if(max< candies[i]){
+                max = candies[i];
+            }
+        }
+
+        for(int i =0; i < candies.size(); i++){
+            if(candies[i]+ extraCandies < max){
+                ans.push_back(false);
+            }
+            else{
+                ans.push_back(true);
+            }
+        }
+        return ans;
+    }
+};
