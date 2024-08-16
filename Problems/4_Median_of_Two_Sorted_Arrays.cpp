451946e00b1430192395c80511b6c7855f8ef308
SayamMahajan <mahajansayam3@gmail.com>
2024-08-16 12:20:42 +0530

    4_Median_of_Two_Sorted_Arrays

---
+++ Problems/4_Median_of_Two_Sorted_Arrays.cpp
@@ -0,0 +1,33 @@
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size();
+        int m = nums2.size();
+        int total = n + m;
+        vector<int> merged(total);
+    
+        int i = 0, j = 0, k = 0;
+        while (i < n && j < m) {
+            if (nums1[i] < nums2[j]) {
+                merged[k++] = nums1[i++];
+            } else {
+                merged[k++] = nums2[j++];
+            }
+        }
+        
+        while (i < n) {
+            merged[k++] = nums1[i++];
+        }
+        while (j < m) {
+            merged[k++] = nums2[j++];
+        }
+        if (total % 2 == 0) {
+            return (merged[total/2 - 1] + merged[total/2]) / 2.0;
+        } else {
+            return merged[total/2];
+        }    
+    }
+};
