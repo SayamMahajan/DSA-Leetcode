4138066de8312af022ae444142680dc0094b5a81
SayamMahajan <mahajansayam3@gmail.com>
2024-08-25 15:26:55 +0530

    1221_Split_a_String_in_Balanced_Strings

---
+++ Problems/1221_Split_a_String_in_Balanced_Strings.cpp
@@ -0,0 +1,23 @@
+#include<string>
+using namespace std;
+
+class Solution {
+public:
+    int balancedStringSplit(string s) {
+        int count = 0;
+        int balance = 0;
+        
+        for(char c : s) {
+            if (c == 'L') {
+                balance++;
+            } else if (c == 'R') {
+                balance--;
+            }
+            if (balance == 0) {
+                count++;
+            }
+        }
+        
+        return count;
+    }
+};
